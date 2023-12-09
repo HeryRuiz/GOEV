a2e40ea5006112c48523e00720e005c82625cb21
Hery Ruiz <heryrdev@gmail.com>
2023-12-09 16:42:21 -0500

    addec overview page

--- src/components/Interior.jsx
+++ src/components/Interior.jsx
@@ -2,11 +2,16 @@ import "../styles/Interior.css";
 import { Link } from "react-router-dom";
 function Interior() {
   return (
-      <section className="interior">
-        <div className="interior__content">
-            
+    <section className="interior">
+      <div className="interior__content">
+        <div className="interior__text">
+          <p className="interior__title">CONNECT WITH YOUR VEHICLE</p>
+          <p className="interior__des">
+            GOEV'S NEXT-GENERATION INTELLIGENT CABIN PROVIDES UNPARALLELED DRIVER-CAR CONNECTION
+          </p>
         </div>
-      </section>
+      </div>
+    </section>
   );
 }
 
